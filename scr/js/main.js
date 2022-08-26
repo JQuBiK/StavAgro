aeb3ee380e6606d142affa6684ff6c14b902c8fa
JQuBiK <111731235+JQuBiK@users.noreply.github.com>
2022-08-26 13:10:18 +0300

    Add files via upload

--- scr/js/main.js
+++ scr/js/main.js
@@ -1,6 +1,34 @@
-let switchBtn = document.getElementsByClassName(".modalWindow__check");
+"use strict";
+let modalWindowBg = document.querySelector('.modalWindowBg');
+let modalWindow = document.querySelector('.modalWindow');
+let openModalbtns = document.querySelectorAll('.openModalWindow');
+let btnCloseWindow = document.querySelector('.modalWindow__closeBtn');
+let btnCheckTechnical = document.querySelector('.modalWindow__check__technical');
+let btnCheckService = document.querySelector('.modalWindow__check__service');
 
-button.addEventListener('click', () => { 
-    switchBtn.classList.toggle('active'); 
-})
-    
+openModalbtns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        modalWindowBg.classList.add('active');
+        modalWindow.classList.add('active');
+    })
+});
+
+btnCloseWindow.addEventListener('click', () => {
+    modalWindowBg.classList.remove('active');
+    modalWindow.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === modalWindowBg) {
+        modalWindowBg.classList.remove('active');
+        modalWindow.classList.remove('active');
+    }
+});
+
+btnCheckTechnical.addEventListener('click', () => {
+    btnCheckTechnical.classList.toggle('active');
+});
+btnCheckService.addEventListener('click', () => {
+    btnCheckService.classList.toggle('active');
+});
